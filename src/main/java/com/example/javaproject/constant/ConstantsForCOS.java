4b45c1cdb398f73ecb361d77f00f46032d85fdba
chenyao <2656571256@qq.com>
2025-04-18 13:22:46 +0800

    测试

---
+++ src/main/java/com/example/javaproject/constant/ConstantsForCOS.java
@@ -0,0 +1,33 @@
+package com.example.javaproject.constant;
+
+public class ConstantsForCOS {
+	/**REGION--北京一区(已售完)*/
+	public static final String REGION_BEIJINGYIQU = "ap-beijing-1";
+	/**REGION--北京*/
+	public static final String REGION_BEIJING = "ap-beijing";
+	/**REGION--南京*/
+	public static final String REGION_NANJING = "ap-nanjing";
+	/**REGION--上海*/
+	public static final String REGION_SHANGHAI = "ap-shanghai";
+	/**REGION--广州*/
+	public static final String REGION_GUANGZHOU = "ap-guangzhou";
+	/**REGION--成都*/
+	public static final String REGION_CHENGDU = "ap-chengdu";
+	/**REGION--重庆*/
+	public static final String REGION_CHONGQING = "ap-chongqing";
+	
+	public static final int MILLISECONDS_TIME_OUT = 30*1000;
+	/**
+	 * secretId
+	 */
+	public static final String SECRET_ID = "AKID7MTO4WjhQu9AxVpulNluoTtKbfU6fz5w";
+	/**
+	 * secretKey
+	 */
+	public static final String SECRET_KEY = "iCRTIsELDqENI1jYHUHxR2WxHTUIsDJA";
+	/**
+	 * bucketName
+	 */
+	public static final String BUCKET_NAME = "xye-card-1252149893";
+	
+}
--- src/main/java/com/example/javaproject/util/COSUtils.java
+++ src/main/java/com/example/javaproject/util/COSUtils.java
@@ -1,6 +1,7 @@
 package com.example.javaproject.util;
 
 
+import com.example.javaproject.constant.ConstantsForCOS;
 import com.qcloud.cos.COSClient;
 import com.qcloud.cos.ClientConfig;
 import com.qcloud.cos.auth.BasicCOSCredentials;
@@ -25,14 +26,12 @@ public class COSUtils {
 	
 	/**获取客户端实例*/
 	public static COSClient getCOSClient() throws RuntimeException{
-		// 1 初始化用户身份信息（secretId, secretKey）
-		// SECRETID 和 SECRETKEY 请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi 进行查看和管理
-		String secretId = "AKID7MTO4WjhQu9AxVpulNluoTtKbfU6fz5w";
-		String secretKey = "iCRTIsELDqENI1jYHUHxR2WxHTUIsDJA";
+		String secretId = ConstantsForCOS.SECRET_ID;
+		String secretKey = ConstantsForCOS.SECRET_KEY;
 		COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
 		// 2 设置 bucket 的地域, COS 地域的简称请参见 https://cloud.tencent.com/document/product/436/6224
 		// clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
-		Region region = new Region("ap-nanjing");
+		Region region = new Region(ConstantsForCOS.REGION_NANJING);
 		ClientConfig clientConfig = new ClientConfig(region);
 		// 这里建议设置使用 https 协议
 		// 从 5.6.54 版本开始，默认使用了 https
