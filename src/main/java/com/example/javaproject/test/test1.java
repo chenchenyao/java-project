f94a826e0b69ecd7d00ccfb442fc7f92a3025fd9
chenyao <2656571256@qq.com>
2025-05-12 13:45:27 +0800

    0512分支测试

---
+++ src/main/java/com/example/javaproject/test/test1.java
@@ -0,0 +1,12 @@
+package com.example.javaproject.test;
+
+/**
+ * @author admin
+ * @Description
+ * @create 2025-05-12 13:42
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("this is 0512  分支");
+    }
+}
